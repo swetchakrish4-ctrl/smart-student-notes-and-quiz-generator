27f296225524e11ddd99b37c06f17be8f5593dbe
swetchakrish4-ctrl <sakshikrish4@gmail.com>
2026-01-15 21:42:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        return "PDF uploaded. Notes and quiz generated."
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
