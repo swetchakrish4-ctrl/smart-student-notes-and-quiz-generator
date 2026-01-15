5d9e6710399a252e564f419cdc708e697cf752a1
swetchakrish4-ctrl <sakshikrish4@gmail.com>
2026-01-15 21:43:42 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,24 @@
 
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    username TEXT,
+    email TEXT
+);
+
+CREATE TABLE documents (
+    document_id INTEGER PRIMARY KEY,
+    file_name TEXT,
+    user_id INTEGER
+);
+
+CREATE TABLE notes (
+    notes_id INTEGER PRIMARY KEY,
+    summary_text TEXT,
+    document_id INTEGER
+);
+
+CREATE TABLE quizzes (
+    quiz_id INTEGER PRIMARY KEY,
+    quiz_content TEXT,
+    document_id INTEGER
+);
